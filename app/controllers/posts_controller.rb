bf99009f7163ec04ab07048f6451f9602faec0d9
Brandon Fonnemann <branjosef@gmail.com>
2020-10-15 14:34:39 -0500

    f

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -52,13 +52,10 @@ class PostsController < ApplicationController
         redirect_to post_path(@post)
        end
 
-
     private
 
         def post_params
             params.require(:post).permit(:title, :content, :likes, :blogger_id, :destination_id)
         end
-
-
 
 end
